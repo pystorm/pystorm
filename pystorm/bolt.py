6b26d70a38dbc3443ab8ac52be69e856da2ddfef
Dan Blanchard <dan.blanchard@gmail.com>
2015-10-20 15:05:58 -0400

    No longer reset _current_tups in finally statement to prevent wrong Tuples from being failed.

--- pystorm/bolt.py
+++ pystorm/bolt.py
@@ -167,23 +167,23 @@ class Bolt(Component):
 
         Separated out so it can be properly unit tested.
         """
-        self._current_tups = [self.read_tuple()]
-        try:
-            tup = self._current_tups[0]
-            if self.is_heartbeat(tup):
-                self.send_message({'command': 'sync'})
-            elif self.is_tick(tup):
-                self.process_tick(tup)
-                if self.auto_ack:
-                     self.ack(tup)
-            else:
-                self.process(tup)
-                if self.auto_ack:
-                     self.ack(tup)
-        finally:
-            # reset so that we don't accidentally fail the wrong Tuples
-            # if a successive call to read_tuple fails
-            self._current_tups = []
+        tup = self.read_tuple()
+        self._current_tups = [tup]
+        if self.is_heartbeat(tup):
+            self.send_message({'command': 'sync'})
+        elif self.is_tick(tup):
+            self.process_tick(tup)
+            if self.auto_ack:
+                 self.ack(tup)
+        else:
+            self.process(tup)
+            if self.auto_ack:
+                 self.ack(tup)
+        # Reset _current_tups so that we don't accidentally fail the wrong
+        # Tuples if a successive call to read_tuple fails.
+        # This is not done in `finally` clause because we want the current
+        # Tuples to fail when there is an exception.
+        self._current_tups = []
 
     def _handle_run_exception(self, exc):
         """Process an exception encountered while running the ``run()`` loop.
