c7ab4bc8e0b3dbdd305a7a156ef58dddaa37296c
Dan Blanchard <dan.blanchard@gmail.com>
2016-04-15 15:16:36 -0400

    Add VERSION and __version__ directly to pystorm namespace

--- pystorm/__init__.py
+++ pystorm/__init__.py
@@ -1,6 +1,13 @@
+'''
+pystorm is a production-tested Storm multi-lang implementation for Python
+
+It is mostly intended to be used by other libraries (e.g., streamparse).
+'''
+
 from .component import Component, Tuple
 from .bolt import BatchingBolt, Bolt, TicklessBatchingBolt
 from .spout import Spout
+from .version import __version__, VERSION
 
 __all__ = [
     'BatchingBolt',
